690f1b2f1e292d57c91e4a339e32d7a930f5f3e9
Alperen Celik <alperenclk1876@gmail.com>
2024-08-04 16:59:06 +0300

    Update same features

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ import pyautogui
 pyautogui.sleep(1)
 
 
-def templateMatching(imagePath, threshold=0.8):
+def templateMatching(imagePath, threshold=0.5):
     findCord = pyautogui.locateOnScreen(imagePath, confidence=threshold)
     resultPoints = pyautogui.center(findCord)
     return resultPoints
 
 
-### Start STREAMING
-def startScreaming():
+# ### Start STREAMING
+# def startScreaming():
 
-    # Specify the folder where the .exe file is located and the file name
-    exe_file_directory = "C:\\Dev\\Tap-To-Earn_Bot_For_Telegram\\scrcpy-win64-v2.4"
-    exe_file_name = "scrcpy.exe"
-    exe_file_path = os.path.join(exe_file_directory, exe_file_name)
+#     # Specify the folder where the .exe file is located and the file name
+#     exe_file_directory = "C:\\Dev\\Tap-To-Earn_Bot_For_Telegram\\scrcpy-win64-v2.4"
+#     exe_file_name = "scrcpy.exe"
+#     exe_file_path = os.path.join(exe_file_directory, exe_file_name)
 
-    # Run .exe file for streaming
-    try:
-        process = subprocess.Popen(
-            [
-                exe_file_path,
-                "--prefer-text",
-                "--turn-screen-off",
-                "--stay-awake",
-            ],  # "--turn-screen-off"
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        print("Exe file executed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while executing the exe file: {e}")
+#     # Run .exe file for streaming
+#     try:
+#         process = subprocess.Popen(
+#             [
+#                 exe_file_path,
+#                 "--prefer-text",
+#                 "--turn-screen-off",
+#                 "--stay-awake",
+#             ],  # "--turn-screen-off"
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#         )
+#         print("Exe file executed successfully.")
+#     except subprocess.CalledProcessError as e:
+#         print(f"An error occurred while executing the exe file: {e}")
 
-    pyautogui.sleep(5)  # Wait for opening screen at 5 sec
+#     pyautogui.sleep(5)  # Wait for opening screen at 5 sec
 
 
 def openTelegramApp(imagePath):
@@ -58,16 +58,17 @@ def openTelegramApp(imagePath):
 def main():
 
     ## start streaming
-    startScreaming()
+    # startScreaming()
 
-    ## Open Telegram
+    # Open Telegram
     # imagePath = "templates\mobile_icon3.png"
     # openTelegramApp(imagePath)
 
     ## Hit the boss
-    imagePath = "templates\\boss_body2.png"
+    imagePath = "templates\\boss_body_huawei.png"
     print("-" * 20 + "Hitting the BOSS" + "-" * 20)
     counter = 3
+    global resultPoint
     while counter > 0:
         try:
             resultPoint = templateMatching(imagePath)
